dd76e62cc3a26b210380f14b2ad57a9f2259ab6f
KingArthur-IT <67517973+KingArthur-IT@users.noreply.github.com>
2021-03-21 18:48:38 +0200

    Адаптация радиуса влияния и числа частиц от разрешения

--- js/main.js
+++ js/main.js
@@ -166,9 +166,11 @@ class Simulation{
     setAnimationProperties() {
         this.cnv.width  = innerWidth;
         this.cnv.height = innerHeight;
+        this.cfg.force_radius = parseInt(innerWidth * 0.02);
+        this.cfg.particlesCount = parseInt(innerWidth * 10);
         if (this.cnv.width < 800) {
-            this.cfg.force_radius = 20;
-            this.cfg.particlesCount = 10000;
+            //this.cfg.force_radius = 20;
+            //this.cfg.particlesCount = 10000;
         };
         this.grid.Initialize(innerWidth, innerHeight, this.cfg.particleColor);
         this.InitializeParticles();
