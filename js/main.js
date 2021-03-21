15480e50e5635746583beb9866ec2550e008c4c2
KingArthur-IT <67517973+KingArthur-IT@users.noreply.github.com>
2021-03-21 20:59:57 +0200

    mobile test 7

--- js/main.js
+++ js/main.js
@@ -170,7 +170,7 @@ class Simulation{
     setAnimationProperties() {
         this.cnv.width  = innerWidth;
         this.cnv.height = innerHeight;
-        if (innerWidth < 400) { this.cfg.force_radius = 5;}
+        if (innerWidth < 400) { this.cfg.force_radius = 15;}
         this.grid.Initialize(innerWidth, innerHeight, this.cfg.particleColor);
         this.InitializeParticles();
     }
@@ -313,7 +313,7 @@ class Simulation{
 
             //get the hsl index of the color
             let hslColor = this.grid.cells[col][row].color;
-            let { R, G, B } = this.hslToRGB(hslColor, 1.0, 0.1);
+            let { R, G, B } = this.hslToRGB(hslColor, 1.0, 0.3);
             this.color[i * 4] = R;
             this.color[i * 4 + 1] = G;
             this.color[i * 4 + 2] = B;
